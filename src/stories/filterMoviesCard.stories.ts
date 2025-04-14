229a6205acdf6d08097dce21d2272da4d1cfd1df
itsjustinjose <itsjustinjose@gmail.com>
2025-04-14 20:37:24 +0100

    Home page components.

---
+++ src/stories/filterMoviesCard.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import FilterMoviesCard from "../components/filterMoviesCard";
+
+const meta = {
+  title: 'Home Page/FilterMoviesCard',
+  component: FilterMoviesCard,
+} satisfies Meta<typeof FilterMoviesCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Basic: Story = {};  // Default story
+Basic.storyName = "Default";
