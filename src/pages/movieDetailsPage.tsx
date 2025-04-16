af8921fe266b6d8ff2771b349575a5452e432cfd
itsjustinjose <itsjustinjose@gmail.com>
2025-04-16 11:10:48 +0100

    Movie Details Page rendering.

---
+++ src/pages/movieDetailsPage.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { MoviePageProps } from "../types/interfaces";
+import MovieHeader from "../components/headerMovie";
+import { Grid, ImageList, ImageListItem } from "@mui/material";
+import MovieDetails from "../components/movieDetails";
+
+const styles = {
+    imageListRoot: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+    },
+    gridListTile: { 
+      width: "100%",
+      height: "auto",
+    },
+  };
+
+  const MoviePage: React.FC<MoviePageProps> = ({movie, images}) => {
+
+    return(
+        <>
+          {movie ? (
+            <>
+              <MovieHeader {...movie} />
+              <Grid container spacing={5} style={{ padding: "15px" }}>
+                <Grid item xs={3}>
+                  <div >
+                    <ImageList sx={styles.imageListRoot} cols={1}>
+                      {images.map((image) => (
+                        <ImageListItem
+                          key={image.file_path}
+                          sx={styles.gridListTile}
+                          cols={1}
+                        >
+                         <img
+                            src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                            alt={'Image alternative'}
+                          />                    
+                        </ImageListItem>
+                      ))}
+                    </ImageList>
+                  </div>
+                </Grid>
+                <Grid item xs={9}>
+                  <MovieDetails {...movie} />
+                </Grid>
+              </Grid>
+            </>
+          ) : (
+            <h2>Waiting for API data</h2>
+          )}
+        </>
+      );
+    };
+
+  export default MoviePage;
